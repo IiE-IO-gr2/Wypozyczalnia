372bfd0da611057a5775d2ad54f1cb6b62f4c094
Krystyna Gołaszewska <krychcia@wp.pl>
2018-01-03 14:23:11 +0100

    Funkcje łączenia z bazą + logowanie na podstawie danych z bazy

---
+++ Wypozyczalnia/Aplikacja wypożyczalnia/FunkcjeSQL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+public static class FunkcjeSQL
+{
+    public static string connString = @"Password=iieIOgr2;Persist Security Info=True;User ID=IOadmin;Initial Catalog=wypozyczalnia;"+
+        @"Data Source=iie-io-gr2.database.windows.net";
+
+    public static DataTable PobierzDaneSQL(string zapytanie, ref string exmsg)
+    {
+        using (SqlConnection conn = new SqlConnection(connString))
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlDataReader dr;
+                SqlCommand sqlc;
+                sqlc = new SqlCommand(zapytanie);
+                sqlc.Connection = conn;
+                conn.Open();
+                dr = sqlc.ExecuteReader();
+                dt.Load(dr);
+                conn.Close();
+                return dt;
+            }
+            catch (SqlException ex)
+            {
+                exmsg += "\nSQL: " + ex.Message + " " + ex.Number + " " + ex.ErrorCode + " " + ex.LineNumber;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                exmsg += "\nSys: " + ex.Message;
+                conn.Close();
+            }
+            return dt;
+        }
+    }
+
+    //Wstawianie danych do bazy
+    //poprzez zapytanie SQL (insert values into...)
+    //exmsg - powinien być try-catch jak w funkcji powyżej
+    public static void WstawDaneSQL(string zapytanie, ref string exmsg)
+    {
+        using (SqlConnection conn = new SqlConnection(connString))
+        {
+            SqlCommand sqlc = new SqlCommand(zapytanie);
+            sqlc.CommandType = CommandType.Text;
+            sqlc.Connection = conn;
+            conn.Open();
+            sqlc.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
--- Wypozyczalnia/Aplikacja wypożyczalnia/Panel_administratora.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/Panel_administratora.cs
@@ -24,9 +24,30 @@ namespace Aplikacja_wypożyczalnia
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            Menu_po_zalogowaniu m = new Menu_po_zalogowaniu();
-            m.Show();
+            string l = textBox1.Text;
+            string h = textBox2.Text;
+            string zapytanie = "Select haslo from [dbo].[Panel_administratora] where Login='"+l+"'";
+            string exmsg="";
+            DataTable dt = FunkcjeSQL.PobierzDaneSQL(zapytanie, ref exmsg);
+            string hasloZBazy = "";
+            foreach (DataRow item in dt.Rows)
+            {
+                hasloZBazy = item[0].ToString();
+            }
+            if (h == hasloZBazy)
+            {
+                this.Hide();
+                Menu_po_zalogowaniu m = new Menu_po_zalogowaniu();
+                m.Show();
+            }
+            else
+            {
+                MessageBox.Show("Błędne dane.");
+                if (!string.IsNullOrEmpty(exmsg))
+                {
+                    MessageBox.Show(exmsg);
+                }
+            }
         }
     }
 }
