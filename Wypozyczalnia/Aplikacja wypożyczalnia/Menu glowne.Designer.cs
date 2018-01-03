dcd47408800ba791a86f5d71e9433469ce6a11be
Patryk Krygiel <patryk___16@tlen.pl>
2018-01-03 11:19:50 +0100

    menu glowne

--- Wypozyczalnia/Aplikacja wypożyczalnia/Menu glowne.Designer.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/Menu glowne.Designer.cs
@@ -29,36 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(52, 73);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Panel administratora";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(195, 18);
+            this.label1.Location = new System.Drawing.Point(183, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(238, 20);
-            this.label1.TabIndex = 1;
+            this.label1.TabIndex = 0;
             this.label1.Text = "Wypożyczalnia Samochodów";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Panel administratora";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(52, 186);
+            this.button2.Location = new System.Drawing.Point(53, 179);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 34);
+            this.button2.Size = new System.Drawing.Size(142, 34);
             this.button2.TabIndex = 2;
             this.button2.Text = "Zamknij aplikację";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,10 +67,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(600, 269);
+            this.ClientSize = new System.Drawing.Size(599, 271);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -81,8 +80,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
--- Wypozyczalnia/Aplikacja wypożyczalnia/Menu glowne.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/Menu glowne.cs
@@ -16,10 +16,5 @@ namespace Aplikacja_wypożyczalnia
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
