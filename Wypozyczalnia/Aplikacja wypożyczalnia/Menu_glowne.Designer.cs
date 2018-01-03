eabd9cbaa06b481c25eb688ca61afc512902c572
Patryk Krygiel <patryk___16@tlen.pl>
2018-01-03 21:24:50 +0100

    zmiana

--- Wypozyczalnia/Aplikacja wypożyczalnia/Menu_glowne.Designer.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/Menu_glowne.Designer.cs
@@ -37,7 +37,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(299, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(348, 29);
