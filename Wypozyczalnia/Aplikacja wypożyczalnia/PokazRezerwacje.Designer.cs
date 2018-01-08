5b82f0266a1d1d0123a5a6458e9a8e3af6dea21c
Ewelina <eewena@gmail.com>
2018-01-08 21:09:15 +0100

    znowu

---
+++ Wypozyczalnia/Aplikacja wypożyczalnia/PokazRezerwacje.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Aplikacja_wypożyczalnia
+{
+    partial class PokazRezerwacje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PokazRezerwacje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Aplikacja_wypożyczalnia.Properties.Resources._26653464_1222776397822724_926715336_o1;
+            this.ClientSize = new System.Drawing.Size(888, 484);
+            this.Name = "PokazRezerwacje";
+            this.Text = "PokazRezerwacje";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
