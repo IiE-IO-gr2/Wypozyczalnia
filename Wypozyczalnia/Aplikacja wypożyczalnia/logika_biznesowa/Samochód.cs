098f0c7be3752254d80f819662c295f38e426bc8
Patryk Krygiel <patryk___16@tlen.pl>
2018-01-15 13:20:52 +0100

    kod na dodanie samochodu

--- Wypozyczalnia/Aplikacja wypożyczalnia/logika_biznesowa/Samochód.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/logika_biznesowa/Samochód.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data;
+using System.Globalization;
 namespace logika_biznesowa {
 	public class Samochód {
 		/// <summary>
@@ -77,9 +79,71 @@ namespace logika_biznesowa {
 		/// <summary>
 		/// metoda dodaj¹ca samochód do bazy
 		/// </summary>
-		public void DodajSamochod() {
-			throw new System.Exception("Not implemented");
-		}
+        public Samochód()
+        {
+            Id_samochodu = 0;
+            Marka = "";
+            Model = "";
+            Pojemnosc = 0.0;
+            Rodzaj_paliwa = "";
+            Typ_nadwozia = "";
+            Ilosc_koni = 0;
+            Skrzynia_biegow = "";
+            Ilosc_biegów = 0;
+            Zu¿ycie_paliwa = 0.0;
+            Ilosc_miejsc = 0;
+            Ilosc_drzwi = 0;
+            Rocznik = 0;
+            Kolor = "";
+            Cena_za_dobê = 0.0;
+            Dostepnosc = false;
+            Inne = "";
+            Kaucja = 0;
+        }
+
+        /// <summary>
+        /// konstruktor Klient
+        /// </summary>
+        public Samochód(int id, string mar, string mod, double poj, string rp, string tn, int ik, string sk, int ib, double zp, int im, int ilodr, int roc, string kol, double czd, bool dostep, string inne, int ka)
+        {
+            Id_samochodu = id;
+            Marka = mar;
+            Model = mod;
+            Pojemnosc = poj;
+            Rodzaj_paliwa = rp;
+            Typ_nadwozia = tn;
+            Ilosc_koni = ik;
+            Skrzynia_biegow = sk;
+            Ilosc_biegów = ib;
+            Zu¿ycie_paliwa = zp;
+            Ilosc_miejsc = im;
+            Ilosc_drzwi = ilodr;
+            Rocznik = roc;
+            Kolor = kol;
+            Cena_za_dobê = czd;
+            Dostepnosc = dostep;
+            Inne = inne;
+            Kaucja = ka;
+        }
+
+        /// <summary>
+        /// metoda dodania klienta do bazy
+        /// </summary>
+        public string DodajSamochod()
+        {
+            string exmsg = "";
+            int dostbool = 0;
+            if (Dostepnosc == true)
+                dostbool = 1;
+            string poj = Pojemnosc.ToString(CultureInfo.CreateSpecificCulture("en-GB"));
+            string zuz = Zu¿ycie_paliwa.ToString(CultureInfo.CreateSpecificCulture("en-GB"));
+            string czd = Cena_za_dobê.ToString(CultureInfo.CreateSpecificCulture("en-GB"));
+            string zapytanie = @"insert into [dbo].[Samochód] ([Id_samochodu], [Marka], [Model], [Pojemnosc], [Rodzaj_paliwa], [Typ_nadwozia], [Ilosc_koni], [Skrzynia_biegow], [Ilosc_biegów], [Zu¿ycie_paliwa], [Ilosc_miejsc], [Ilosc_drzwi], [Rocznik], [Kolor], [Cena_za_dobê], [Dostepnosc], [Inne], [Kaucja] )" +
+                @"values (" + Id_samochodu + ", '" + Marka + "' , '" + Model + "', " + Pojemnosc + ", '" + Rodzaj_paliwa + "', '" + Typ_nadwozia + "', " + Ilosc_koni + ", '" + Skrzynia_biegow + "', " + Ilosc_biegów + ", " + zuz + ", " + Ilosc_miejsc + ", " + Ilosc_drzwi + ", " + Rocznik + ", '" + Kolor + "', " + czd + ", " + dostbool + ", '" + Inne + "', " + Kaucja + " )";
+            FunkcjeSQL.WstawDaneSQL(zapytanie, ref exmsg);
+            return exmsg;
+        }
+		
 		/// <summary>
 		/// metoda edytuj¹ca samochód w bazie
 		/// </summary>
@@ -89,8 +153,37 @@ namespace logika_biznesowa {
 		/// <summary>
 		/// metoda usuwaj¹ca samochód z bazy
 		/// </summary>
-		public void UsunSamochod() {
-			throw new System.Exception("Not implemented");
+        public static string UsunSamochod(int identyfikator)
+        {
+            string zapytanieCzySamochodIstnieje = @"SELECT count(*) FROM [dbo].[Samochód] WHERE [Id_samochodu] = " + identyfikator;
+            string exmsgTest = "";
+            string zwrotZapytanieCzySamochodIstnieje = FunkcjeSQL.PobierzDaneSQLPojedyncze(zapytanieCzySamochodIstnieje, ref exmsgTest);
+            if (!string.IsNullOrWhiteSpace(exmsgTest)) // zapytanie testuj¹ce, czy w bazie jest samochód o danym ID zwróci³o b³¹d
+                return exmsgTest;
+            else // zapytanie nie zwróci³o b³êdu
+            {
+                int licznik;
+                if (int.TryParse(zwrotZapytanieCzySamochodIstnieje, out licznik) == true) // uzyskan¹ wartoœæ da siê przekonwetowaæ na inta
+                {
+                    if (licznik == 1) // zapytanie zwróci³o znalezienie w bazie samochodów rekordu o podanym ID
+                    {
+                        string exmsg = "", exmsg1 = "", exmsg2 = "";
+                        // usuniêcie danych z tabeli Samochody
+                        string zapytanie1 = @"UPDATE [dbo].[Samochód] SET [CzyUsuniete] = 1 WHERE [Id_klienta] = " + identyfikator;
+                        FunkcjeSQL.WstawDaneSQL(zapytanie1, ref exmsg1);
+                        // budowa informacji wyjœciowej z funkcji
+                        if (!string.IsNullOrWhiteSpace(exmsg1))
+                            exmsg += "\n" + exmsg1;
+                        if (!string.IsNullOrWhiteSpace(exmsg2))
+                            exmsg += "\n" + exmsg2;
+                        return exmsg;
+                    }
+                    else
+                        return "Nie odnaleziono samochodu o podanym ID";
+                }
+                else
+                    return "Nie odnaleziono samochodu o podanym ID";
+            }
 		}
 		/// <summary>
 		/// metoda wyszukuj¹ca samochód w bazie
@@ -109,6 +202,20 @@ namespace logika_biznesowa {
 
 		private Rezerwacja rezerwacja;
 
+        /// <summary>
+        /// Metoda pobieraj¹ca z bazy danych najwy¿szy dotychczas uzyty numer ID
+        /// </summary>
+        /// <returns></returns>
+        public static int MaksymalnyNumerIdentyfikatoraWBazie()
+        {
+            string zapytanie = @"select max([Id_samochodu]) from [dbo].[Samochód]";
+            string exmsg = "";
+            string numerZBazy = FunkcjeSQL.PobierzDaneSQLPojedyncze(zapytanie, ref exmsg);
+            if (!string.IsNullOrWhiteSpace(numerZBazy))
+                return int.Parse(numerZBazy);
+            else
+                return 0;
+        }
 	}
 
 }
