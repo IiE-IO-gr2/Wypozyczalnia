7ad30f5007406a9a165ed32bb94ba4de7c109a55
Bernadeta Amanowicz <bernadeta.amanowicz@gmail.com>
2018-01-21 21:30:18 +0100

    jakiesformydowyp

---
+++ Wypozyczalnia/Aplikacja wypożyczalnia/Wybierzsamochod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using logika_biznesowa;
+using Aplikacja_wypożyczalnia;
+
+namespace Aplikacja_wypożyczalnia
+{
+    public partial class Wybierzsamochod : Form
+    {
+        string nazwa;
+        public Wybierzsamochod()
+        {
+            
+            InitializeComponent();
+            System.Data.DataTable exmsg_kl = FunkcjePomicnicze.PobierzDaneSQL("SELECT * FROM  Klient_firmy", ref nazwa);
+           // Klient_firmy klientwyswietl = new Klient_firmy(nazwa, nip);
+           //dataGridView1.Rows.Add(id, nazwa, nip, tl);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DodajWypozyczenie dw = new DodajWypozyczenie();
+            dw.Show();
+        }
+    }
+}
