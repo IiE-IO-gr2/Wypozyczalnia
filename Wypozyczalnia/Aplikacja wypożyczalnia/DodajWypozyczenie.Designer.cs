b3197926d51b98bb6231ec916ce941fe8e79a479
Paulina Czerniawska <paulinaczerniawskaa@gmail.com>
2018-01-23 22:46:56 +0100

    zmiany

--- Wypozyczalnia/Aplikacja wypożyczalnia/DodajWypozyczenie.Designer.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/DodajWypozyczenie.Designer.cs
@@ -199,7 +199,6 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 49;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label7
             // 
