2f80d9e06e47f541719bb31a861a4c96b31ba1ed
Ewelina <eewena@gmail.com>
2018-01-23 23:20:32 +0100

    fdg

--- Wypozyczalnia/Aplikacja wypożyczalnia/WyszukajKlienta.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/WyszukajKlienta.cs
@@ -27,23 +27,48 @@ namespace Aplikacja_wypożyczalnia
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
+            /// Sprawdzenie poprawności danych w textboxie
+            string bladWTextboxach = "";
+            bool poprawneTextboxy = true;
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                !System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, @"^[0-9]{1,10}$"))
             {
-                int id = int.Parse(textBox1.Text);
-                string exmsg = "";
-                Klient znaleziony = Klient.WyszukajKlienta(ref exmsg);
-                if (!string.IsNullOrWhiteSpace(exmsg))
-                    MessageBox.Show("Wystąpił błąd:\n" + exmsg);
-                else
-                {
-                    PokazWyszukanegoKlientaFirme pokazKlientaFirme = new PokazWyszukanegoKlientaFirme();
-                }
+                bladWTextboxach += "\n\t-Błędna lub pusta wartość w polu ID";
+                poprawneTextboxy = false;
+            }
 
-            }
-            catch (Exception)
+            if (poprawneTextboxy == true)
             {
-                MessageBox.Show("Wystąpił błąd:\nNiepoprawny lub pusty numer identyfikacyjny");
+                /// Pobranie danych z TextBoxa
+                int id1 = int.Parse(textBox1.Text);
+
+                try
+                {
+                    int id = int.Parse(textBox1.Text);
+                    string exmsg = "";
+                    Klient znaleziony = Klient.WyszukajKlienta(ref exmsg);
+                    if (!string.IsNullOrWhiteSpace(exmsg))
+                        MessageBox.Show("Wystąpił błąd:\n" + exmsg);
+                    else
+                    {
+                        PokazWyszukanegoKlientaFirme pokazKlientaFirme = new PokazWyszukanegoKlientaFirme();
+                    }
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Wystąpił błąd:\nNiepoprawny lub pusty numer identyfikacyjny");
+                }
             }
+
+            else
+            {
+                MessageBox.Show("Wystąpił błąd w danych wejściowych:" + bladWTextboxach);
+                bladWTextboxach = "";
+                poprawneTextboxy = true;
+            }
+
+
         }
     }
 }
