38f17999b7c9d950efbbb85c4f5a0a109b4be9d8
Paulina Czerniawska <paulinaczerniawskaa@gmail.com>
2018-01-23 23:04:37 +0100

    coś

--- Wypozyczalnia/Aplikacja wypożyczalnia/Dodaj_rezerwacje.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/Dodaj_rezerwacje.cs
@@ -23,50 +23,81 @@ namespace Aplikacja_wypożyczalnia
 
         }
 
+
+        // Do przetestowania!
+        // DateTime dataPlanowanegoWypozyczenia = dateTimePicker1.Value;
+        //   if (dataPlanowanegoWypozyczenia.CompareTo(DateTime.Today) == -1)
+        //  {
+        //      MessageBox.Show("Nie można podać minionej daty.");
+        //  }
+
         private void wstecz_Click(object sender, EventArgs e)
         {
             this.Hide();
             Rezerwacja re = new Rezerwacja();
             re.Show();
-        
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            // Do przetestowania!
-            DateTime dataPlanowanegoWypozyczenia = dateTimePicker1.Value;
-            if (dataPlanowanegoWypozyczenia.CompareTo(DateTime.Today) == -1)
-            {
-                MessageBox.Show("Nie można podać minionej daty.");
-            }
-
-            /// Sprawdzenie poprawności danych w textboxach
-            string bladWTextboxach = "";
-            bool poprawneTextboxy = true;
-
-            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
-                !System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, @"^[0-9]{1,10}$" ))
-            {
-                bladWTextboxach += "\n\t-Błędna lub pusta wartość w polu ID rezerwacji";
-                poprawneTextboxy = false;
-            }
-            else
-            {
-                MessageBox.Show("Wystąpiły błędy w danych wejściowych:" + bladWTextboxach);
-                bladWTextboxach = "";
-                poprawneTextboxy = true;
-            }
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
+    
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            using (WybierzSamoch ws = new WybierzSamoch())
+            {
+                if (ws.ShowDialog() == DialogResult.OK)
+                {
+                    textBox4.Text = ws.PobraneIDSamWartosc;
+                }
+                else
+                {
+                    MessageBox.Show("Wystąpił nieoczekiwany błąd.");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (WybierzKlienta wk = new WybierzKlienta())
+            {
+                if (wk.ShowDialog() == DialogResult.OK)
+                {
+                    textBox3.Text = wk.PobraneIDKlientaWartosc;
+                }
+                else
+                {
+                    MessageBox.Show("Wystąpił nieoczekiwany błąd.");
+                }
+            }
 
         }
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (WybierzKlientaIndywidualnego wki = new WybierzKlientaIndywidualnego())
+            {
+                if (wki.ShowDialog() == DialogResult.OK)
+                {
+                    textBox3.Text = wki.pobraneIDKlientaWartosc;
+                }
+                else
+                {
+                    MessageBox.Show("Wystąpił nieoczekiwany błąd.");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
     }
 }
