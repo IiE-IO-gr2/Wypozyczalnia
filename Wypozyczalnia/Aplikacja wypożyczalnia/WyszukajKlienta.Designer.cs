76f2f21492edb6514ffc8510017b96245919779e
Ewelina <eewena@gmail.com>
2018-01-24 14:01:06 +0100

    wyszukaj rezerwacje

--- Wypozyczalnia/Aplikacja wypożyczalnia/WyszukajKlienta.Designer.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/WyszukajKlienta.Designer.cs
@@ -99,6 +99,7 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(783, 80);
             this.dataGridView1.TabIndex = 54;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // WyszukajKlienta
             // 
