7d0d635995466b5947ed6d7befdef15a37ba7332
Krystyna Gołaszewska <krychcia@wp.pl>
2018-01-25 03:48:19 +0100

    Wyszukiwanie opóźniony zwrotów

--- Wypozyczalnia/Aplikacja wypożyczalnia/PokazWypozyczenia.Designer.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/PokazWypozyczenia.Designer.cs
@@ -32,6 +32,7 @@
             this.wstecz = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,13 +73,24 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(372, 41);
+            this.button1.Location = new System.Drawing.Point(348, 41);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 29);
+            this.button1.Size = new System.Drawing.Size(238, 29);
             this.button1.TabIndex = 52;
-            this.button1.Text = "Aktualizuj";
+            this.button1.Text = "Aktualizuj listę wypożyczeń";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(592, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 29);
+            this.button2.TabIndex = 53;
+            this.button2.Text = "Wskaż opóźnione zwroty";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // PokazWypozyczenia
             // 
@@ -86,6 +98,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Aplikacja_wypożyczalnia.Properties.Resources._26653464_1222776397822724_926715336_o1;
             this.ClientSize = new System.Drawing.Size(888, 484);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.wstecz);
@@ -104,5 +117,6 @@
         private System.Windows.Forms.Button wstecz;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
