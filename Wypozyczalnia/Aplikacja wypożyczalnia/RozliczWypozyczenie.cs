35a5f7431c7dfd98201f6cea0e6f8f81011cfb5c
Ewelina <eewena@gmail.com>
2018-01-25 15:07:18 +0100

    zm

--- Wypozyczalnia/Aplikacja wypożyczalnia/RozliczWypozyczenie.cs
+++ Wypozyczalnia/Aplikacja wypożyczalnia/RozliczWypozyczenie.cs
@@ -72,37 +72,54 @@ namespace Aplikacja_wypożyczalnia
                         MessageBox.Show("Wystąpił błąd:\n\t-" + exmsg);
                     else
                     {
-                        Rozliczenie rozliczenie1 = new Rozliczenie(dataZwrotu, uszkodzony);
-                        string test = "";
-                        double nowaCena = rozliczenie1.WyliczCene(dt, ref test);
-                        if (nowaCena <= 0)
+                        /// Sprawdzanie, jaka była data wypożyczenia
+                        string dataWypozyczeniaSTR = "";
+                        foreach (DataRow item in dt.Rows)
                         {
-                            MessageBox.Show("Wystąpił błąd podczas wyliczania ceny");
+                            dataWypozyczeniaSTR = item[1].ToString();
+                        }
+                        string dw_rok = string.Concat(dataWypozyczeniaSTR[0], dataWypozyczeniaSTR[1], dataWypozyczeniaSTR[2], dataWypozyczeniaSTR[3]);
+                        string dw_m = string.Concat(dataWypozyczeniaSTR[5], dataWypozyczeniaSTR[6]);
+                        string dw_d = string.Concat(dataWypozyczeniaSTR[8], dataWypozyczeniaSTR[9]);
+                        DateTime dataWypozyczenia = new DateTime(int.Parse(dw_rok), int.Parse(dw_m), int.Parse(dw_d));
+                        if (dataZwrotu.CompareTo(dataWypozyczenia) == -1)
+                        {
+                            MessageBox.Show("Wysapił błąd w danych wejściowych:\n\t-Data zwrotu nie może być wczesniejsza od daty wypożyczenia");
                         }
                         else
                         {
-                            string Data_zwr = dataZwrotu.Year.ToString() + "-" + dataZwrotu.Month.ToString() + "-" + dataZwrotu.Day.ToString();
-                            int idKlienta = -1;
-                            foreach (DataRow item in dt.Rows)
+                            Rozliczenie rozliczenie1 = new Rozliczenie(dataZwrotu, uszkodzony);
+                            string test = "";
+                            double nowaCena = rozliczenie1.WyliczCene(dt, ref test);
+                            if (nowaCena <= 0)
                             {
-                                idKlienta = int.Parse(item[6].ToString());
+                                MessageBox.Show("Wystąpił błąd podczas wyliczania ceny");
                             }
-                            string exmsg_update1 = "";
-                            string zapytanie_update1 = @"update [dbo].[Wypożyczenie] set[CzyRozliczone] = 1, " + 
-                                @"[Data_planowanego_zwrotu] = '" + Data_zwr + "' , [Cena_za_wypozyczenie] = " + nowaCena.ToString() +
-                                @"where[ID_wypożyczenia] = " + id;
-                            FunkcjePomicnicze.WstawDaneSQL(zapytanie_update1, ref exmsg_update1);
-                            string exmsg_update2 = "";
-                            string zapytanie_update2 = @"update [dbo].[Klient] set[Aktywnosc] = [Aktywnosc] + 1" +
-                                @"where[Id_klienta] = " + idKlienta;
-                            FunkcjePomicnicze.WstawDaneSQL(zapytanie_update2, ref exmsg_update2);
-                            if (!string.IsNullOrWhiteSpace(exmsg_update1))
-                                MessageBox.Show("Wystąpił błąd podczas zmiany danych w wypozyczeniu:\n\t-" + exmsg_update1);
-                            if (!string.IsNullOrWhiteSpace(exmsg_update2))
-                                MessageBox.Show("Wystąpił błąd podczas zmiany danych w kliencie:\n\t-" + exmsg_update2);
-                            if (string.IsNullOrWhiteSpace(exmsg_update1) & string.IsNullOrWhiteSpace(exmsg_update2))
-                                MessageBox.Show("Wypożyczenie zostało rozliczone.");
-                            textBox3.Text = nowaCena.ToString();
+                            else
+                            {
+                                string Data_zwr = dataZwrotu.Year.ToString() + "-" + dataZwrotu.Month.ToString() + "-" + dataZwrotu.Day.ToString();
+                                int idKlienta = -1;
+                                foreach (DataRow item in dt.Rows)
+                                {
+                                    idKlienta = int.Parse(item[6].ToString());
+                                }
+                                string exmsg_update1 = "";
+                                string zapytanie_update1 = @"update [dbo].[Wypożyczenie] set[CzyRozliczone] = 1, " +
+                                    @"[Data_planowanego_zwrotu] = '" + Data_zwr + "' , [Cena_za_wypozyczenie] = " + nowaCena.ToString() +
+                                    @"where[ID_wypożyczenia] = " + id;
+                                FunkcjePomicnicze.WstawDaneSQL(zapytanie_update1, ref exmsg_update1);
+                                string exmsg_update2 = "";
+                                string zapytanie_update2 = @"update [dbo].[Klient] set[Aktywnosc] = [Aktywnosc] + 1" +
+                                    @"where[Id_klienta] = " + idKlienta;
+                                FunkcjePomicnicze.WstawDaneSQL(zapytanie_update2, ref exmsg_update2);
+                                if (!string.IsNullOrWhiteSpace(exmsg_update1))
+                                    MessageBox.Show("Wystąpił błąd podczas zmiany danych w wypozyczeniu:\n\t-" + exmsg_update1);
+                                if (!string.IsNullOrWhiteSpace(exmsg_update2))
+                                    MessageBox.Show("Wystąpił błąd podczas zmiany danych w kliencie:\n\t-" + exmsg_update2);
+                                if (string.IsNullOrWhiteSpace(exmsg_update1) & string.IsNullOrWhiteSpace(exmsg_update2))
+                                    MessageBox.Show("Wypożyczenie zostało rozliczone.");
+                                textBox3.Text = nowaCena.ToString();
+                            }
                         }
                     }
                 }
